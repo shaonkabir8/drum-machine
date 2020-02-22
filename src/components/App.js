3bac26bbe6c9636c0a7543e396d8969cf8830569
Shaonkabir <shaonkabir98@gmail.com>
2020-02-22 22:34:48 +0600

    Refactor: add function to play audio on [keyDown] event

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,26 @@ import '../styles/App.scss';
 
 const App = () => {
 
+  // function for playing sound on 'keydown' event
+  const playSound = (e) => {
+    // grab our audio elements
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    
+    // check if audio exists or not!
+    if (!audio) return;
+
+    // If exists,
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.type = 'audio/wav';
+    audio.play();
+  }
+  // TODO: 
+  // This will be modified later while deploying on Netlify
+  // set listener for window Object
+  window.addEventListener('keydown', playSound);
+
   return ( 
     <Fragment>
       <div className="keys">
