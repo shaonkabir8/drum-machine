40545dae21e617338eb8d388b345986c1e5b1c6c
Shaonkabir <shaonkabir98@gmail.com>
2020-02-23 11:45:28 +0600

    Refactor: add sidebar power controller

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import '../styles/App.scss';
 
 const App = () => {
+  
+  
+  const [power, setPower] = useState(true)
+  
+
+  // function to controll power
+  const togglePower = (e) => {
+    setPower(!power);
+    
+    // check if power is true or not!
+    power ? e.target.style = 'float: left' : e.target.style = 'float: right';  
+  }
+
+
 
   // function for playing sound on 'keydown' event
   const playSound = (e) => {
@@ -35,59 +49,95 @@ const App = () => {
   }, [])
 
 
-  return ( 
+  return (
     <Fragment>
       <div className="drum-container">
         <div className="keys">
-        <div data-key="81" className="key">
-          <kbd>Q</kbd>
-          <span className="sound">clap</span>
+          <div data-key="81" className="key">
+            <kbd>Q</kbd>
+            <span className="sound">clap</span>
+          </div>
+          <div data-key="87" className="key">
+            <kbd>W</kbd>
+            <span className="sound">hihat</span>
+          </div>
+          <div data-key="69" className="key">
+            <kbd>E</kbd>
+            <span className="sound">kick</span>
+          </div>
+          <div data-key="65" className="key">
+            <kbd>A</kbd>
+            <span className="sound">openhat</span>
+          </div>
+          <div data-key="83" className="key">
+            <kbd>S</kbd>
+            <span className="sound">boom</span>
+          </div>
+          <div data-key="68" className="key">
+            <kbd>D</kbd>
+            <span className="sound">ride</span>
+          </div>
+          <div data-key="90" className="key">
+            <kbd>Z</kbd>
+            <span className="sound">snare</span>
+          </div>
+          <div data-key="88" className="key">
+            <kbd>X</kbd>
+            <span className="sound">tom</span>
+          </div>
+          <div data-key="67" className="key">
+            <kbd>C</kbd>
+            <span className="sound">tink</span>
+          </div>
         </div>
-        <div data-key="87" className="key">
-          <kbd>W</kbd>
-          <span className="sound">hihat</span>
-        </div>
-        <div data-key="69" className="key">
-          <kbd>E</kbd>
-          <span className="sound">kick</span>
-        </div>
-        <div data-key="65" className="key">
-          <kbd>A</kbd>
-          <span className="sound">openhat</span>
-        </div>
-        <div data-key="83" className="key">
-          <kbd>S</kbd>
-          <span className="sound">boom</span>
-        </div>
-        <div data-key="68" className="key">
-          <kbd>D</kbd>
-          <span className="sound">ride</span>
-        </div>
-        <div data-key="90" className="key">
-          <kbd>Z</kbd>
-          <span className="sound">snare</span>
-        </div>
-        <div data-key="88" className="key">
-          <kbd>X</kbd>
-          <span className="sound">tom</span>
-        </div>
-        <div data-key="67" className="key">
-          <kbd>C</kbd>
-          <span className="sound">tink</span>
+
+        <div className="sidebar-controller">
+          <div className="control">
+            <p>Power</p>
+            <div className="select">
+              <div className="toggle-power" onClick={e => togglePower(e)} style={{'float': 'right'}}></div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
 
-    <audio data-key="81" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio>
-    <audio data-key="87" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"></audio>
-    <audio data-key="69" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"></audio>
-    <audio data-key="65" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"></audio>
-    <audio data-key="83" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"></audio>
-    <audio data-key="68" src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"></audio>
-    <audio data-key="90" src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"></audio>
-    <audio data-key="88" src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"></audio>
-    <audio data-key="67" src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"></audio>
-  </Fragment>
+      <audio
+        data-key="81"
+        src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+      ></audio>
+      <audio
+        data-key="87"
+        src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+      ></audio>
+      <audio
+        data-key="69"
+        src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+      ></audio>
+      <audio
+        data-key="65"
+        src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+      ></audio>
+      <audio
+        data-key="83"
+        src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+      ></audio>
+      <audio
+        data-key="68"
+        src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+      ></audio>
+      <audio
+        data-key="90"
+        src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+      ></audio>
+      <audio
+        data-key="88"
+        src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+      ></audio>
+      <audio
+        data-key="67"
+        src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+      ></audio>
+    </Fragment>
   );
 }
 
